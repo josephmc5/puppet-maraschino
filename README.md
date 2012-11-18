b9ef781c143a92ea9285559cba815878584ed185
josephmc5 <josephmc5>
2012-11-18 09:31:00 -0500

    config through script now

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ puppet-maraschino
 
 Install maraschino 
 
-No config file so you will have to configure it manually. Complain here: http://forums.maraschinoproject.com/showthread.php?tid=143
--- manifests/config.pp
+++ manifests/config.pp
@@ -1,17 +1,32 @@
 class maraschino::config {
+    package { 'sqlite3':
+        ensure  => present,
+    }
     file { "$base_dir/maraschino/data/":
         ensure => directory,
         owner => 'maraschino',
         group => 'maraschino',
     }
-#    file { "$base_dir/maraschino/config.ini":
-#        content => template('maraschino/config.ini.erb'),
-#        owner => 'maraschino',
-#        group => 'maraschino',
-#        mode => '0644',
-#        require => File["$base_dir/maraschino/config/"],
-#        notify => Service['supervisor::maraschino'],
-#    }
+    file { "$base_dir/maraschino/config.sh":
+        content => template('maraschino/config.sh.erb'),
+        owner => 'maraschino',
+        group => 'maraschino',
+        mode => '0755',
+        notify => Exec['sqlite3-config'],
+    }
+    file { 'sqlite3-check':
+        content => template('maraschino/config_check.sh.erb'),
+        path    => "$base_dir/maraschino/config_check.sh",
+        owner => 'maraschino',
+        group => 'maraschino',
+        mode => '0755',
+    }
+    exec { 'sqlite3-config':
+        command     => "$base_dir/maraschino/config.sh",
+        unless      => "$base_dir/maraschino/config_check.sh",
+        path        => '/usr/bin',
+        require     => [Package['sqlite3'],File['sqlite3-check']],
+    }
     
     file { "$log_dir":
         ensure => directory,
